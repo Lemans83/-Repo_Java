2fa5f57cf8ab5a6410612fc2b2d3be182cc26467
Lemans K <lemansk@Lemanss-Air.home>
2022-03-05 15:32:45 -0500

    Mysecondhomework

---
+++ src/Myjavapackage/Myhomework.java
@@ -0,0 +1,40 @@
+package Myjavapackage;
+
+public class Myhomework {
+
+
+    public static void main(String[] args) {
+
+        byte NumberOfHoursInaDay = 24;
+        System.out.println("Number of hours in a day " + NumberOfHoursInaDay );
+
+        short maximumNumberInAYear = 365;
+        System.out.println("Maximum number in a year " + maximumNumberInAYear);
+
+        byte TotalNumberOfEmployeesInAnOrganization = 30;
+        System.out.println("Total number of employees in an organization " + TotalNumberOfEmployeesInAnOrganization);
+
+        double populationInACountry = 1000000.00;
+        System.out.println("Population in a country " +populationInACountry);
+
+        double Mortgagerate = 12.99;
+        System.out.println("Mortgage rate " + Mortgagerate);
+
+        double Balanceinabankaccount = 99.99;
+        System.out.println("Balance in a bank account " + Balanceinabankaccount);
+
+        boolean DoesTheSunRiseFromTheWest = false;
+        System.out.println("Does the sun rise from the west: " + DoesTheSunRiseFromTheWest);
+
+        char Myinitial = 'M';
+
+        char Lastnameinitial = 'K';
+        System.out.println("\nMy initial is " + Myinitial + Lastnameinitial);
+
+        String Fullname = "My full name is Masrur Karimov";
+        System.out.println("My full name is Masrur Karimov");
+
+    }
+
+
+}
