661878714e1ecd6172fc457a5669144efbce6016
Lemans K <lemansk@Lemanss-Air.home>
2022-03-11 14:49:18 -0500

    Practice

--- src/javaoperatorspractice/Comparisonclass.java
+++ src/javaoperatorspractice/Comparisonclass.java
@@ -3,6 +3,12 @@ package javaoperatorspractice;
 public class Comparisonclass {
 
     public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++); // prints 10
+        System.out.println(i++); // print 11
 
+        int b = 20;
+        b++;
+        System.out.println(b); // prints 21
     }
 }
---
+++ src/javaoperatorspractice/Conceptstringclass.java
@@ -0,0 +1,4 @@
+package javaoperatorspractice;
+
+public class Conceptstringclass {
+}
