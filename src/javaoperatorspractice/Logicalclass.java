96a5a94d3635bb7794475a145b8314aa53f70f7c
Lemans K <lemansk@Lemanss-Air.home>
2022-03-12 15:01:04 -0500

    Practice

--- src/javaoperatorspractice/Logicalclass.java
+++ src/javaoperatorspractice/Logicalclass.java
@@ -3,9 +3,55 @@ package javaoperatorspractice;
 public class Logicalclass {
     public static void main(String[] args) {
 
+        /**
+         * Logical (&&, II, !)
+         *
+         * && -> And operator
+         * II -> Or operator
+         * ! -> Not operator
+         *
+         * We use the logical operators to join two or more conditions.
+         * The final result will be always "Boolean datatype"
+         *
+         * && operator (and operator)
+         * all combined conditions must be true to get final result as true
+         * if any one of the conditions is false, final result is false.
+         *
+         * || operator (or operator)
+         * any one of the combined conditions has to be true to get final result as true
+         * if all combined conditions are false, then final result is false.
+         *
+         * ! operator (not operator)
+         * not operator will make true as false and vice versa
+         */
 
+        int lVar1=10, lVar2=20, lVar3=30, lVar4=40;
 
+        boolean lRes1 = (lVar2 == lVar4+lVar3) && lVar1 <= lVar3;
+        /*
+            (lVar2 == lVar4+lVar3) && lVar1 <= lVar3
+            (20 == 40+30) && 10 <= 30
+            (20 == 70) && 10 <= 30
+            false && true
+            false
+         */
+        System.out.println("lRes1 = " + lRes1);
 
+        double Var1=15.6;
+        double Var2=16.7;
+        double Var3=20;
+        double Var4=53.5;
+        double Var5=55;
+        boolean reSult = (Var1 == Var3+Var2) || Var4 >= Var5;
+        System.out.println(reSult);
+
+        int case1 = 50;
+        int case2 = 20;
+        double case3 = 25.6;
+        double case4 = 40.3;
+        int case5 = 30;
+        boolean reSult1 = case1 >= case5+case2 && case3 == case4 || case4 > case5;
+        System.out.println(reSult1);
 
     }
 }
--- src/javaoperatorspractice/Ternaryclass.java
+++ src/javaoperatorspractice/Ternaryclass.java
@@ -1,4 +1,42 @@
 package javaoperatorspractice;
 
 public class Ternaryclass {
+
+    public static void main(String[] args) {
+
+        /**
+         * Ternary (?:)
+         *
+         * Syntax
+         * Variable = condition(s) ? ValueIfTrue : ValueIfFalse
+         */
+        //mortgageRate = 2.5/3.5 (creditScore > 700 / Otherwise ) - Example
+        //mortgageRate = 2.5/3.5 (creditScore > 700 or you are first time buyer / Otherwise )
+
+        double myMoney = 500.5;
+        String Result = myMoney > 400 ? "I am right" : "I am wrong";
+        System.out.println(Result);
+
+        int myBank = 10000;
+        int result2 = myBank > 5000 ? 345 : 255;
+        System.out.println(myBank);
+
+        int nhb = 88;
+        String result3 = nhb > 90 ? "True" : "False";
+        System.out.println(result3);
+
+        double myCheck = 350.5;
+        String result4 = myCheck < 250? "Ture" : "False";
+        System.out.println(result4);
+
+        int mybalance = 6000000;
+        String result5 = mybalance < 300000? "True" : "False";
+        System.out.println(result5);
+
+        double myaccount = 10.5;
+        String result6 = myaccount > 9.5? "True" : "False";
+        System.out.println(result6);
+
+
+    }
 }
